9b868ebce7554baa93204d3d36e3400819d57598
Isayevv10 <85827814+Isayevv10@users.noreply.github.com>
2022-07-05 11:17:57 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -4,6 +4,20 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+        let num = parseInt(result.input);
+        function prime(num) {
+            if (num <= 1) { 
+                return false;
+            } else {
+                for (var i = 2; i < num; i++) {
+                    if (num % i === 0) {
+                        return false; 
+                    }
+                }
+                return true;
+            }  
+        }
+        
+        console.log(prime(num));
   
 });
