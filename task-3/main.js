6a0d799c26ff016e13d2bbcfe1de2cd6ca72121d
Isayevv10 <85827814+Isayevv10@users.noreply.github.com>
2022-07-05 11:28:33 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -4,6 +4,10 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+        let arr = result.input.split(',');
+        let arr2 = arr.reverse();
+        for (let item of arr2) {
+            console.log(item);
+        }
   
 });
