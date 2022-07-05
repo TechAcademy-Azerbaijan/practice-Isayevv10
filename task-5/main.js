d90857ecc672e7d120526f64003d3ef0601b95c2
Isayevv10 <85827814+Isayevv10@users.noreply.github.com>
2022-07-05 12:01:58 +0400

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -4,6 +4,10 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    let a = result.input.split(' '); 
+    a.sort( (a,b) => {
+        return b - a;
+    })
+    console.log(a[0]);
   
 });
